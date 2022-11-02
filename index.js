cf098d1122ad87613f370891242f2c928edc6b93
Kevin <kevinfiorentino.1294@gmail.com>
2022-11-02 18:30:19 -0300

    Change server response

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const http = require('http');
 
 const port = 5000;
 
-const server = http.createServer((req, res) => {
-	console.log('pid', process.pid);
-	
+const server = http.createServer((req, res) => {	
 	res.statusCode = 200;
   	res.setHeader('Content-Type', 'application/json');
-  	res.end(JSON.stringify({ message: 'Reverse proxy NodeJS - NGINX' }));
+  	res.end(JSON.stringify({ message: `Reverse proxy NodeJS/NGINX - Process ID = ${process.pid}` }));
 });
 
 server.listen(port, () => {
